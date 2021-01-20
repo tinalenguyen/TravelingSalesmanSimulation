72d491d786c31d184a64f0a0a64029175dd8f785
SHKim10 <skim10@stuy.edu>
2021-01-19 23:59:43 -0500

    Implemented idea for 2D array to hold distances between cities

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -10,9 +10,21 @@ public class TravelingSalesman{
       int [][] distancesList ;
       for (int i = 0 ; nextLine.hasNext() ; i++){
 
-
       }
+    }
 
-  }
+      //create a 2d array of cities and assign cities
+      ArrayList<String> allCities = new ArrayList<String>();
+      int[][] distances = new int[allCities.size()][allCities.size()];
+      Scanner city = new Scanner(); //add a scanner
+      while (city.hasNextLine()){
+        Scanner line = new Scanner(city.nextLine());
+        while (line.hasNext()){
+          String cityOne = line.next();
+          String cityTwo = line.next();
+          int distance = line.nextInt();
+          distances[allCities.indexOf(city1)][allCities.indexOf(city2)] = distance;
+        }
+      }
 
 }
