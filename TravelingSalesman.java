17490d38d2655801a6b97ae5e76dffbec2bab8b8
tinalenguyen <>
2021-01-19 22:38:18 -0500

    started scanner

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -1,8 +1,18 @@
 import java.util.*;
 
-public class TraveingSalesman{
+public class TravelingSalesman{
 
   public static int[][] readFile(String text){
+      ArrayList<String> cities = new ArrayList<String>();
+      File texts = new File(Input.txt);
+      Scanner input = new Scanner(texts);
+      Scanner nextLine = new Scanner(input.nextLine());
+      int [][] distancesList ;
+      for (int i = 0 ; nextLine.hasNext() ; i++){
+
+
+      }
 
   }
+
 }
