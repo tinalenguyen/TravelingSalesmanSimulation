1622866cb156f86e81f5a9da48a984711d6fabbe
Victor Veytsman <vityavv@gmail.com>
2021-01-20 21:50:04 -0500

    Add scanner function

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -1,30 +1,51 @@
 import java.util.*;
+import java.io.*;
 
 public class TravelingSalesman{
+	public static int[][] readFile(String filename) throws FileNotFoundException {
+		// Note: I did some reading and found an interesting Scanner method called "skip"
+		// We could potentially use this method to skip "=" and "to"
+		// However, I decided not to include it for simplicity
 
-  public static int[][] readFile(String text){
-      ArrayList<String> cities = new ArrayList<String>();
-      File texts = new File(Input.txt);
-      Scanner input = new Scanner(texts);
-      Scanner nextLine = new Scanner(input.nextLine());
-      int [][] distancesList ;
-      for (int i = 0 ; nextLine.hasNext() ; i++){
+		ArrayList<String> cities = new ArrayList<String>();
+		// Doing one pass over the text to get all of the city names
+		// This could potentially be made faster by using nested array lists (ArrayList<ArrayList<Integer>>) and adding cities as they are encountered
+		File f = new File(filename);
+		Scanner input = new Scanner(f);
+		while (input.hasNextLine()) {
+			Scanner line = new Scanner(input.nextLine());
+			if (!line.hasNext()) break; //encountered an empty line
+			// Could be made robust by including error checking for improper lines
+			// Or by just looping through all of the words in the line and throwing out "to," "=," and numbers
+			// But this was the easiest way for now
+			String city = line.next();
+			if (!cities.contains(city)) cities.add(city);
+			line.next(); //discard "to"
+			city = line.next();
+			if (!cities.contains(city)) cities.add(city);
+		}
 
-      }
-    }
-
-      //create a 2d array of cities and assign cities
-      ArrayList<String> allCities = new ArrayList<String>();
-      int[][] distances = new int[allCities.size()][allCities.size()];
-      Scanner city = new Scanner(); //add a scanner
-      while (city.hasNextLine()){
-        Scanner line = new Scanner(city.nextLine());
-        while (line.hasNext()){
-          String cityOne = line.next();
-          String cityTwo = line.next();
-          int distance = line.nextInt();
-          distances[allCities.indexOf(city1)][allCities.indexOf(city2)] = distance;
-        }
-      }
-
+		// Second pass to actually collect data
+		int[][] distances = new int[cities.size()][cities.size()];
+		f = new File(filename); // This might not be necessary if Scanner doesn't consume the file. Added it just to be safe.
+		input = new Scanner(f); // From the top
+		while (input.hasNextLine()) {
+			Scanner line = new Scanner(input.nextLine());
+			while (line.hasNext()) { // Copied from older code but not sure of its necessity
+				String cityOne = line.next();
+				line.next(); //discard to
+				String cityTwo = line.next();
+				line.next(); //discard =, since nextInt doesn't skip to the next int but just takes the next token and throws java.util.InputMismatchException otherwise
+				int distance = line.nextInt();
+				distances[cities.indexOf(cityOne)][cities.indexOf(cityTwo)] = distance;
+				distances[cities.indexOf(cityTwo)][cities.indexOf(cityOne)] = distance;
+			}
+		}
+		return distances;
+	}
+	public static void main(String[] args) throws FileNotFoundException { // throws is temporary here
+		// So far, main just tests the readFile function
+		int[][] distances = readFile("Input.txt");
+		System.out.println(Arrays.deepToString(distances)); // A todo could be to implement more robust checks
+	}
 }
